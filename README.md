c0d65c5ceabcc2829692186b9279e86cd35d0bf2
wenderli <wenderli_ricardoo@hotmail.com>
2026-03-09 15:14:49 -0300

    add Prettier and `lint:check` and `lint:fix` for code formatting and update Home component styles

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do clone https://www.tanews.com.br para aprimoramento profissional
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,18 +8,18 @@ export default function Home() {
       </header>
 
       <section>
-        <Image 
-          src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjcwY3Vxd21zOGwzNjJ5OGN5a2lldDd6MTFyajR2cW84d2YxOHQ1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/t8xgPfC5oNIRMrNooe/giphy.gif" 
-          alt="Um lindo buquê de flores animado" 
-          width={480} 
+        <Image
+          src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjcwY3Vxd21zOGwzNjJ5OGN5a2lldDd6MTFyajR2cW84d2YxOHQ1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/t8xgPfC5oNIRMrNooe/giphy.gif"
+          alt="Um lindo buquê de flores animado"
+          width={480}
           height={480}
           unoptimized={true}
-          style={{ 
-            maxWidth: '100%', 
-            height: 'auto', 
-            borderRadius: '15px', 
-            boxShadow: '0 4px 8px rgba(0,0,0,0.1)' 
-          }} 
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+            borderRadius: "15px",
+            boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+          }}
         />
       </section>
     </main>
