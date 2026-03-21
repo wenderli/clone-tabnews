16c0dc87b68ff81b1afee275cb41b4329b90c98e
wenderli <wenderli_ricardoo@hotmail.com>
2026-03-21 16:27:51 -0300

    Fix client connection handling in database query function

--- infra/database.js
+++ infra/database.js
@@ -8,14 +8,15 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     await client.end();
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
