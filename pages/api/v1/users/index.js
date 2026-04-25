17be7b8cfc85598703d9da81b362dd88f20dfbaf
wenderli <wenderli_ricardoo@hotmail.com>
2026-04-24 22:25:06 -0300

    feat: implement user activation email functionality and corresponding tests

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 import user from "models/user.js";
+import activation from "models/activation.js";
 
 const router = createRouter();
 
@@ -11,5 +12,8 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInputValues = request.body;
   const newUser = await user.create(userInputValues);
+
+  await activation.sendEmailToUser(newUser);
+
   return response.status(201).json(newUser);
 }
